8f2a77417b6d1b281918f040dfec0248ffa4c708
Nitespring <96525113+Nitespring@users.noreply.github.com>
2024-04-01 12:46:49 +0200

    Working on Bullet Functionality

--- src/main/java/github/nitespring/darkestsouls/common/entity/projectile/Bullet.java
+++ src/main/java/github/nitespring/darkestsouls/common/entity/projectile/Bullet.java
@@ -3,25 +3,43 @@ package github.nitespring.darkestsouls.common.entity.projectile;
 import github.nitespring.darkestsouls.common.entity.projectile.throwable.ThrowingKnifeEntity;
 import github.nitespring.darkestsouls.common.entity.util.CustomBlockTags;
 import github.nitespring.darkestsouls.core.init.ItemInit;
+import net.minecraft.core.particles.ParticleOptions;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.AbstractHurtingProjectile;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.EntityHitResult;
+import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.fluids.FluidType;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Random;
 
 public class Bullet extends AbstractHurtingProjectile {
 
     protected float damage;
     protected int poiseDamage;
-    protected int pierce;
-    protected int ricochet;
+    //protected int pierce;
+    //protected int ricochet;
+    protected int hitEntities;
+    public int gravTick;
+    protected int hitBlocks;
     protected int flyingTime;
     protected static final EntityDataAccessor<Float> SIZE = SynchedEntityData.defineId(ThrowingKnifeEntity.class, EntityDataSerializers.FLOAT);
+    protected static final EntityDataAccessor<Integer> PIERCE = SynchedEntityData.defineId(ThrowingKnifeEntity.class, EntityDataSerializers.INT);
+    protected static final EntityDataAccessor<Integer> RICOCHET = SynchedEntityData.defineId(ThrowingKnifeEntity.class, EntityDataSerializers.INT);
     public Bullet(EntityType<? extends AbstractHurtingProjectile> e, Level l) {
         super(e, l);
     }
@@ -29,8 +47,8 @@ public class Bullet extends AbstractHurtingProjectile {
         super(e, l);
         this.damage = damageIn;
         this.poiseDamage = poiseIn;
-        this.pierce = pierceIn;
-        this.ricochet = ricochetIn;
+        this.setPierce(pierceIn);
+        this.setRicochet(ricochetIn);
         this.flyingTime = flyingTime;
     }
 
@@ -40,19 +58,28 @@ public class Bullet extends AbstractHurtingProjectile {
     public void setPoiseDamage(int poiseDamage) {this.poiseDamage = poiseDamage;}
     public int getFlyingTime() {return flyingTime;}
     public void setFlyingTime(int flyingTime) {this.flyingTime = flyingTime;}
-    public int getPierce() {return pierce;}
-    public void setPierce(int pierce) {this.pierce = pierce;}
+    public int getPierce() {return entityData.get(PIERCE);}
+    public void setPierce(int pierce) {entityData.set(PIERCE,pierce);}
     public float getSize() {return entityData.get(SIZE);}
     public void setSize(float size){entityData.set(SIZE,size);}
     @Override
-    protected void defineSynchedData() {this.entityData.define(SIZE, 0.4f);}
-    public int getRicochet() {return ricochet;}
-    public void setRicochet(int ricochet) {this.ricochet = ricochet;}
+    protected void defineSynchedData() {
+        this.entityData.define(SIZE, 0.4f);
+        this.entityData.define(RICOCHET, 0);
+        this.entityData.define(PIERCE, 0);
+    }
+    public int getRicochet() {return entityData.get(RICOCHET);}
+    public void setRicochet(int ricochet) {entityData.set(RICOCHET,ricochet);}
 
     @Override
     protected void onHitEntity(EntityHitResult p_37259_) {
         super.onHitEntity(p_37259_);
-        this.discard();
+        Entity e = p_37259_.getEntity();
+        e.hurt(e.level().damageSources().mobProjectile(this, (LivingEntity) this.getOwner()), this.damage);
+        if(hitEntities>=getPierce()){
+            this.discard();
+        }
+        hitEntities++;
     }
     @Override
     protected void onHitBlock(BlockHitResult result) {
@@ -62,16 +89,39 @@ public class Bullet extends AbstractHurtingProjectile {
             this.level().destroyBlock(result.getBlockPos(), true, this.getOwner());
             level().gameEvent(this, GameEvent.BLOCK_DESTROY, result.getBlockPos());
         }else {
-            this.discard();
+            hitBlocks++;
+            if (hitBlocks >= getRicochet()) {
+                this.discard();
+                this.getOwner().level().playSound((Player)null, this.position().x(),this.position().y(),this.position().z(), SoundEvents.STONE_PLACE, SoundSource.AMBIENT, 0.2F, 1.6f);
+            } else {
+                Vec3 mov = this.getDeltaMovement();
+                Random r = new Random();
+                float r1 = 2 * (r.nextFloat() - 0.5f);
+                float r2 = 2 * (r.nextFloat() - 0.5f);
+                float r3 = 2 * (r.nextFloat() - 0.5f);
+                this.setDeltaMovement(-mov.x * (0.6 - 0.4 * r1), -mov.y * (0.6 - 0.4 * r2), -mov.z * (0.6 - 0.4 * r3));
+                this.xPower = -xPower * (0.6 - 0.4 * r1);
+                this.yPower = -yPower * (0.6 - 0.4 * r2);
+                this.zPower = -zPower * (0.6 - 0.4 * r3);
+                this.getOwner().level().playSound((Player)null, this.position().x(),this.position().y(),this.position().z(), SoundEvents.ANVIL_HIT, SoundSource.AMBIENT, 0.2F, 1.6f);
+                gravTick=0;
+            }
         }
     }
     @Override
     public void tick() {
         super.tick();
+        gravTick++;
+        this.setDeltaMovement(this.getDeltaMovement().add(0, -0.0005 * Math.pow(gravTick, 9/4), 0));
     }
-
     @Override
     public boolean isOnFire() {
         return false;
     }
+    @Nullable
+    @Override
+    protected ParticleOptions getTrailParticle() {
+        return super.getTrailParticle();
+    }
+    
 }
--- src/main/java/github/nitespring/darkestsouls/common/item/child/guns/Pistol.java
+++ src/main/java/github/nitespring/darkestsouls/common/item/child/guns/Pistol.java
@@ -4,6 +4,8 @@ import github.nitespring.darkestsouls.common.entity.projectile.Bullet;
 import github.nitespring.darkestsouls.common.entity.projectile.throwable.FirebombEntity;
 import github.nitespring.darkestsouls.common.item.Gun;
 import github.nitespring.darkestsouls.core.init.EntityInit;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
@@ -32,7 +34,8 @@ public class Pistol extends Gun{
 
         Bullet entity = new Bullet(EntityInit.BULLET.get(), level);
         entity.setPos(x,y,z);
-        float flyingPower = this.flyingPower(player, stackIn);
+        //float flyingPower = this.flyingPower(player, stackIn);
+        float flyingPower = 0.5f;
         entity.xPower=flyingPower*aim.x;
         entity.yPower=flyingPower*aim.y;
         entity.zPower=flyingPower*aim.z;
@@ -40,11 +43,15 @@ public class Pistol extends Gun{
         entity.setAttackDamage(this.getAttackDamage(player, stackIn));
         entity.setPoiseDamage(this.getPoiseDamage(player, stackIn));
         entity.setSize(this.getBaseSize());
-        entity.setPierce(this.getPierce(player, stackIn));
-        entity.setRicochet(this.getRicochet(player, stackIn));
+        //entity.setPierce(this.getPierce(player, stackIn));
+        entity.setPierce(2);
+        //entity.setRicochet(this.getRicochet(player, stackIn));
+        entity.setRicochet(10 );
         level.addFreshEntity(entity);
 
         player.getCooldowns().addCooldown(stackIn.getItem(), this.getUseCooldown(player, stackIn));
+
+        //player.level().playSound((Player)null, player, SoundEvents.GENERIC_EXPLODE, SoundSource.PLAYERS, 1.0F, 1.0f);
 
         return super.use(level, player, hand);
     }
