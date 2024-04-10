3abc27c4c166e60f75f52ad4799be2aae335ffb1
Nitespring <96525113+Nitespring@users.noreply.github.com>
2024-04-10 10:44:18 +0200

    Starting Work on Church Doctor

---
+++ src/main/java/github/nitespring/darkestsouls/client/render/entity/mob/church/doctor/ChurchDoctorItemLayer.java
@@ -0,0 +1,76 @@
+package github.nitespring.darkestsouls.client.render.entity.mob.church.doctor;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Axis;
+import github.nitespring.darkestsouls.common.entity.mob.church.ChurchDoctor;
+import github.nitespring.darkestsouls.common.entity.mob.hollow.*;
+import github.nitespring.darkestsouls.core.init.ItemInit;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.world.item.ItemDisplayContext;
+import net.minecraft.world.item.ItemStack;
+import software.bernie.geckolib.animatable.GeoEntity;
+import software.bernie.geckolib.cache.object.GeoBone;
+import software.bernie.geckolib.renderer.GeoRenderer;
+import software.bernie.geckolib.renderer.layer.BlockAndItemGeoLayer;
+
+public class ChurchDoctorItemLayer<T extends ChurchDoctor & GeoEntity> extends BlockAndItemGeoLayer<T>{
+
+	public ChurchDoctorItemLayer(GeoRenderer<T> renderer) {
+		super(renderer);
+		
+	}
+	
+	
+
+	@Override
+	protected ItemStack getStackForBone(GeoBone bone, T animatable) {
+		 if (bone.getName().equals("itemRight")) {
+				 return animatable.getRightHandItem();
+		 }else if (bone.getName().equals("itemLeft")) {
+			 return animatable.getLeftHandItem();
+		 }else{
+				 return null;
+			 }
+		 }
+	
+	@Override
+	protected ItemDisplayContext getTransformTypeForStack(GeoBone bone, ItemStack stack, T animatable) {
+		if (bone.getName().equals("itemRight")) {
+			  return ItemDisplayContext.THIRD_PERSON_LEFT_HAND;
+		  }else if (bone.getName().equals("itemLeft")) {
+			return ItemDisplayContext.THIRD_PERSON_LEFT_HAND;
+		}else {
+			return null;
+		}
+
+	}
+	
+	
+
+
+
+@Override
+protected void renderStackForBone(PoseStack poseStack, GeoBone bone, ItemStack stack, T animatable,
+		MultiBufferSource bufferSource, float partialTick, int packedLight, int packedOverlay) {
+	poseStack.scale(0.9f,0.9f,0.9f);
+	if (bone.getName().equals("right_item")) {
+
+			poseStack.translate(0, 0, 0);
+			poseStack.mulPose(Axis.XP.rotationDegrees(0));
+			poseStack.mulPose(Axis.YP.rotationDegrees(0));
+			poseStack.mulPose(Axis.ZP.rotationDegrees(0));
+	  }
+	if (bone.getName().equals("left_item")) {
+			poseStack.translate(0, 0, 0);
+			poseStack.mulPose(Axis.XP.rotationDegrees(0));
+			poseStack.mulPose(Axis.YP.rotationDegrees(0));
+			poseStack.mulPose(Axis.ZP.rotationDegrees(0));
+
+
+	}
+	super.renderStackForBone(poseStack, bone, stack, animatable, bufferSource, partialTick, packedLight, packedOverlay);
+}
+	
+
+
+}
---
+++ src/main/java/github/nitespring/darkestsouls/common/entity/mob/church/ChurchDoctorStick.java
@@ -0,0 +1,11 @@
+package github.nitespring.darkestsouls.common.entity.mob.church;
+
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.PathfinderMob;
+import net.minecraft.world.level.Level;
+
+public class ChurchDoctorStick extends ChurchDoctor{
+    public ChurchDoctorStick(EntityType<? extends PathfinderMob> p_21683_, Level p_21684_) {
+        super(p_21683_, p_21684_);
+    }
+}
