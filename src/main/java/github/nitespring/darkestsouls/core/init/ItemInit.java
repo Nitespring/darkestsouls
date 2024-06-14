42c928cf8d5a9e89254cb38327fa29e7a7d97b36
Nitespring <96525113+Nitespring@users.noreply.github.com>
2024-06-14 12:35:47 +0200

    Fixed Update

--- src/main/java/github/nitespring/darkestsouls/core/init/ItemInit.java
+++ src/main/java/github/nitespring/darkestsouls/core/init/ItemInit.java
@@ -20,6 +20,7 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 
 import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.SpawnEggItem;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SlabBlock;
 import net.minecraft.world.level.block.SoundType;
